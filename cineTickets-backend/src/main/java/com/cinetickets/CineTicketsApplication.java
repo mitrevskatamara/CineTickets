148af098233bb86cca1ee3e7cd29eb9456017cf2
mitrevskatamara <mitrevskaat@gmail.com>
2022-03-23 12:36:14 +0100

    Initial commit

---
+++ cineTickets-backend/src/main/java/com/cinetickets/CineTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.cinetickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineTicketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CineTicketsApplication.class, args);
+    }
+
+}
---
+++ cineTickets-backend/src/test/java/com/cinetickets/CineTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinetickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineTicketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
